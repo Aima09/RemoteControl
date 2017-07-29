710a5af7e971f610d9bffb63a9fa0f5a9a5b0fba
sujuntao <51217389@qq.com>
2017-07-29 15:39:42 +0800

    同步fileMessage

--- minalibrary/src/main/java/com/yf/minalibrary/message/FileMessage.java
+++ minalibrary/src/main/java/com/yf/minalibrary/message/FileMessage.java
@@ -1,7 +1,5 @@
 package com.yf.minalibrary.message;
 
-import android.util.Base64;
-
 import com.yf.minalibrary.common.FileHelper;
 
 import java.io.File;
@@ -41,19 +39,27 @@ public class FileMessage extends BaseMessage {
         private int fileSize = 0;
         private byte[] fileContent;
 
-        public FileBean() {
+        public FileBean(String senderId, String receiverId,String filePath) {
+            this.senderId = senderId;
+            this.receiverId = receiverId;
+            init(filePath);
         }
 
         public FileBean(String filePath) {
+            init(filePath);
+        }
+
+        private void init(String filePath){
             File file = new File(filePath);
-            FileHelper helper = new FileHelper();
-            fileName = file.getName();
-            fileSize = (int) file.length();
-            try {
-                fileContent = helper.getContent(file);
-                String a = Base64.encodeToString(fileContent,Base64.DEFAULT);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (file.exists()){
+                FileHelper helper = new FileHelper();
+                fileName = file.getName();
+                fileSize = (int) file.length();
+                try {
+                    fileContent = helper.getContent(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
